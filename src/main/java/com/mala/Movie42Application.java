1301e14d3c4a25da8872a8060becce94e80ab2bb
jperucca <under.64@gmail.com>
2016-01-28 23:22:27 +0100

    Initial commit.

---
+++ src/main/java/com/mala/Movie42Application.java
@@ -0,0 +1,12 @@
+package com.mala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie42Application.class, args);
+	}
+}
